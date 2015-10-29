dde67e0e08c90e77c17e77cbfc48ac5678fac7b8
Kyle Potts <pottsk@purdue.edu>
2015-10-29 19:16:30 -0400

    progress

--- src/purduedir/core.clj
+++ src/purduedir/core.clj
@@ -14,20 +14,20 @@
   (html/html-resource (java.net.URL. url)))
 
 
-(defn getName
+(defn getContent 
   [row]
-  (let [name (map html/text (html/select row [:h2.cn-name]))]
-    (apply str name )))
-
-(defn getAlias
-  [row]
-  (let []
-    1))
+  (let [header (map html/text (html/select row [:th])) data (map html/text (html/select row [:td]))]
+    {:header (apply str header) :data (apply str data)}))
 
 (defn handle-table-row
   [row]
-  (let [name (getName row)]
-    (println name)))
+  (let [content (html/select row [:tr])]
+    (map getContent content)))
+
+(defn callDir
+  []
+  (let [tables (html/select (fetch-url (createURL "Kyle")) [:table])]
+        (map handle-table-row tables)))
 
 (defn -main
   [& args]
