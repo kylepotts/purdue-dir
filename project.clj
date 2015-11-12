e74a104d00eb169a501aceb451813a4fe6d78152
Kyle <pottsk@purdue.edu>
2015-11-12 13:12:36 -0500

    add procfile

--- project.clj
+++ project.clj
@@ -4,8 +4,11 @@
                  [enlive "1.1.6"]
                  [org.clojure/data.json "0.2.6"]
                  [ring "1.4.0"]
-                 [compojure "1.4.0"]]
+                 [compojure "1.4.0"]
+                 [environ "1.0.1"]]
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
-  :plugins [[lein-ring "0.9.7"]]
+  :plugins [[lein-ring "0.9.7"]
+            [lein-environ "1.0.1"]]
   :aot [purduedir.core]
+  :min-lein-version "2.0.0"
   :ring {:handler purduedir.server/app})
